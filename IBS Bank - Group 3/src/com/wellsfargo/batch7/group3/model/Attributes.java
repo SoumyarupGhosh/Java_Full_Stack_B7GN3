5e9b0ec6c53b4b704a36bd6be8ea4591b31d0fa2
Soumyarup <70505398+SoumyarupGhosh@users.noreply.github.com>
2020-11-03 16:26:35 +0530

    Delete Attributes.java

--- IBS Bank - Group 3/src/com/wellsfargo/batch7/group3/model/Attributes.java
+++
@@ -1,7 +0,0 @@
-package com.wellsfargo.batch7.group3.model;
-
-public class Attributes {
-	
-	public static final String GET_ACCT_BALANCE = "select AVLBL_BAL from CUST_ACCT where CUST_ACCT_NUM = ?";
-
-}
