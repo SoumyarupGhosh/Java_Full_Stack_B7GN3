545bedc76371d13b1423dd2061ee5d91fee370e4
Soumyarup <70505398+SoumyarupGhosh@users.noreply.github.com>
2020-11-03 16:27:59 +0530

    Delete IFundsTransfer.java

--- IBS Bank - Group 3/src/com/wellsfargo/batch7/group3/view/IFundsTransfer.java
+++
@@ -1,8 +0,0 @@
-package com.wellsfargo.batch7.group3.view;
-
-import com.wellsfargo.batch7.group3.model.CustomerOperations;
-import com.wellsfargo.batch7.group3.model.IBSException;
-
-public interface IFundsTransfer {
-	CustomerOperations transferFunds(CustomerOperations custAcct) throws IBSException;
-}
